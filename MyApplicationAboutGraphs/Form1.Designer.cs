a824b15b09a5dd4ae48dd0c34c22547f53facd53
Dan <ppxy@ya.ru>
2014-08-07 21:07:24 +0400

    Init commit
    Design done

---
+++ MyApplicationAboutGraphs/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace MyApplicationAboutGraphs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelEdges = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelVertices = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonFindPath = new System.Windows.Forms.Button();
+            this.buttonMinSpanTree = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(600, 343);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 361);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(794, 89);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(618, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INFORMATION";
+            // 
+            // labelEdges
+            // 
+            this.labelEdges.AutoSize = true;
+            this.labelEdges.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelEdges.Location = new System.Drawing.Point(618, 34);
+            this.labelEdges.Name = "labelEdges";
+            this.labelEdges.Size = new System.Drawing.Size(56, 17);
+            this.labelEdges.TabIndex = 3;
+            this.labelEdges.Text = "Edges:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(618, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Possible vertices:";
+            // 
+            // labelVertices
+            // 
+            this.labelVertices.AutoSize = true;
+            this.labelVertices.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelVertices.Location = new System.Drawing.Point(618, 51);
+            this.labelVertices.Name = "labelVertices";
+            this.labelVertices.Size = new System.Drawing.Size(80, 17);
+            this.labelVertices.TabIndex = 5;
+            this.labelVertices.Text = "Vertices:";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(621, 103);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(185, 60);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonFindPath
+            // 
+            this.buttonFindPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFindPath.Location = new System.Drawing.Point(621, 295);
+            this.buttonFindPath.Name = "buttonFindPath";
+            this.buttonFindPath.Size = new System.Drawing.Size(185, 60);
+            this.buttonFindPath.TabIndex = 7;
+            this.buttonFindPath.Text = "FIND SHORT PATH";
+            this.buttonFindPath.UseVisualStyleBackColor = true;
+            // 
+            // buttonMinSpanTree
+            // 
+            this.buttonMinSpanTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinSpanTree.Location = new System.Drawing.Point(621, 199);
+            this.buttonMinSpanTree.Name = "buttonMinSpanTree";
+            this.buttonMinSpanTree.Size = new System.Drawing.Size(185, 60);
+            this.buttonMinSpanTree.TabIndex = 8;
+            this.buttonMinSpanTree.Text = "MIN SPAN TREE";
+            this.buttonMinSpanTree.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 462);
+            this.Controls.Add(this.buttonMinSpanTree);
+            this.Controls.Add(this.buttonFindPath);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelVertices);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelEdges);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.canvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Grapher v0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelEdges;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelVertices;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonFindPath;
+        private System.Windows.Forms.Button buttonMinSpanTree;
+    }
+}
+
